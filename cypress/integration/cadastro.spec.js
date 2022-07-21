72cccc9d8ea44b450ae9a5458249e0bac5e7dfd0
Sher Dias <karlianesirotzky@gmail.com>
2022-07-21 01:41:59 -0300

    atualizacao do projeto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -44,7 +44,46 @@ describe('Cadastro' , () => {
 
     })
 
+    it('Usuario digita cpf incorreto' , () =>{
+      
 
+        var entregador = {
+            nome: 'Carlos Alberto Sampaio Dias',
+            cpf: '06404444313aa',
+            email: 'carlosalberto@gmail.com',
+            whatsapp:'85997855477',
+            endereco:{
+                cep:'60525622',
+                rua:'Valdemar Holanda',
+                numero:'215',
+                complemento:'baixos',
+                bairro:'Joao XXIII',
+                cidade:'Fortaleza/CE'
+
+            },
+
+            metodo_entrega:'Moto',
+            cnh: 'cnh-digital.jpg'
+
+                 
+
+
+        }
+        
+        var signup = new SignupPages()
+        signup.go()
+        signup.fillFormCad(entregador)
+        
+        
+        // cy.get('.swal2-container .swal2-html-container')
+            // .should('be.visible', expectedMessage) 
+          //const - objeto que nao vai mudar!
+
+    
+
+
+
+    })
    
 
 
--- cypress/pages/SignupPages.js
+++ cypress/pages/SignupPages.js
@@ -32,11 +32,35 @@ class SignupPages {
         cy.get('form button[type="submit"]').click()
     }
 
- 
+   fillFormCad(entregador) {
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
 
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)      
+
+        
+        cy.get('input[name="address"]').should('be.visible', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('be.visible', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('be.visible', entregador.endereco.city_uf)
+        
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click() //função 'contains' serve para juntar um localizador css com texto de forma mais critériosa
+        cy.get('input[accept^="image"]').attachFile('/images/'+ entregador.cnh)
+        cy.get('form button[type="submit"]').click()
+        
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+
+    }      
     
 
-
+    
 }
 
 export default SignupPages
